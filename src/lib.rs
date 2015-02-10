4d1f61188af4b2f994f6669d0225e0ca100115b8
Potpourri <pot_pourri@mail.ru>
2015-02-10 14:32:41 +0300

    Fix old opengl context init.

--- src/lib.rs
+++ src/lib.rs
@@ -77,8 +77,12 @@ impl GlfwWindow {
 
         // Make sure we have the right GL version.
         glfw.window_hint(glfw::WindowHint::ContextVersion(major as u32, minor as u32));
-        glfw.window_hint(glfw::WindowHint::OpenglForwardCompat(true));
-        glfw.window_hint(glfw::WindowHint::OpenglProfile(glfw::OpenGlProfileHint::Core));
+        if opengl >= OpenGL::_3_0 {
+            glfw.window_hint(glfw::WindowHint::OpenglForwardCompat(true));
+        }
+        if opengl >= OpenGL::_3_2 {
+            glfw.window_hint(glfw::WindowHint::OpenglProfile(glfw::OpenGlProfileHint::Core));
+        }
         glfw.window_hint(glfw::WindowHint::Samples(settings.samples as u32));
 
         // Create GLFW window.
